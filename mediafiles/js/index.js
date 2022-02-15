5ddadf016189d5e570aef01f2a0315a26a90ad7e
mdquilindo <mdquilindo@unicauca.edu.co>
2022-02-14 21:04:44 -0500

    config deploy

--- mediafiles/js/index.js
+++ mediafiles/js/index.js
@@ -1,18 +1,47 @@
 function onFavorite(id_unsplash = '', preview = '') {
-  console.log(id_unsplash, preview);
   fetch('/favorite/add/', {
     method: 'POST',
     body: JSON.stringify({ id_unsplash, preview, favorite: true }),
-  }).then(
-      $('img[id="' + id_unsplash + '"]').attr('src', '/static/img/star_1.png')
-  );
+  }).then((res) => {
+    res.json().then((data) => {
+      if (data.status === 200) {
+        $('img[id="' + id_unsplash + '"]').attr('src', '/static/img/star_1.png');
+        $('img[id="' + id_unsplash + '"]').attr(
+          'onclick',
+          'onUnfavorite("' + id_unsplash + '", "' + preview + '")'
+        );
+        showMessage('Marked as favorite');
+      } else {
+        showMessage('Failed, action not success');
+      }
+    });
+  });
 }
 
-function onUnfavorite(id_unsplash = '') {
-    fetch('/favorite/add/', {
-      method: 'POST',
-      body: JSON.stringify({ id_unsplash, preview: '', favorite: false }),
-    }).then(
-        $('img[id="' + id_unsplash + '"]').attr('src', '/static/img/star_2.png')
-    );
-  }
+function onUnfavorite(id_unsplash = '', preview = '') {
+  fetch('/favorite/add/', {
+    method: 'POST',
+    body: JSON.stringify({ id_unsplash, preview: '', favorite: false }),
+  }).then((res) => {
+    res.json().then((data) => {
+      if (data.status === 200) {
+        $('img[id="' + id_unsplash + '"]').attr('src', '/static/img/star_2.png');
+        $('img[id="' + id_unsplash + '"]').attr(
+          'onclick',
+          'onFavorite("' + id_unsplash + '", "' + preview + '")'
+        );
+        showMessage('Unmarked as favorite');
+      } else {
+        showMessage('Failed, action not success');
+      }
+    });
+  });
+}
+
+function showMessage(message = '') {
+  $('#message').text(message);
+  $('#message').removeClass('hidden');
+  setTimeout(() => {
+    $('#message').addClass('hidden');
+  }, 2000);
+}
